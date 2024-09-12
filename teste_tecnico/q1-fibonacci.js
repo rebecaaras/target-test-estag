f8bc8f6c8fb4955accd5dc174d749b93bc58ffef
rebecaaras <rebecaaraujo2013@gmail.com>
2024-09-11 22:05:19 -0300

    Renomeia arquivos

--- teste_tecnico/q1-fibonacci.js
+++ teste_tecnico/q1-fibonacci.js
@@ -1,3 +1,6 @@
+//Importações
+const prompt = require('prompt-sync')();
+
 fibArray = [0, 1, 1]
 
 //gera a sequencia de fib até um número igual ou superior ao input
@@ -19,4 +22,15 @@ function isFibbonaci(n){
         isFibbonaci(n)
     }
 }
-isFibbonaci(147)
+
+function __main__(){
+    console.log('Verifique se um número pertence à sequencia de Fibonacci.')
+    console.log(`O programa executará 10 vezes`)
+    for (i = 10; i > 0; i--){
+        const n = prompt('Digite um número inteiro:');
+        isFibbonaci(parseInt(n))
+        console.log('')
+    }
+}
+
+__main__()
---
+++ teste_tecnico/q2-string.js
@@ -0,0 +1,18 @@
+//Importações
+const prompt = require('prompt-sync')();
+
+function containsLetter(word){
+    //placeholder
+}
+
+function __main__(){
+    console.log(`Verifique se a letra 'a' existe na string digitada.`)
+    console.log(`(O programa executará 10 vezes)`)
+    for (i = 10; i > 0; i--){
+        const n = prompt('Digite uma string:');
+        
+        console.log('')
+    }
+}
+
+__main__()
