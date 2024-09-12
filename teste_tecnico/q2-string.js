a18aa8da7c8a19e7f38113a3da3d78d0f8ee357e
rebecaaras <rebecaaraujo2013@gmail.com>
2024-09-11 22:51:00 -0300

    Adiciona resposta da segunda questão

--- teste_tecnico/q2-string.js
+++ teste_tecnico/q2-string.js
@@ -1,16 +1,32 @@
 //Importações
 const prompt = require('prompt-sync')();
 
-function containsLetter(word){
-    //placeholder
+function containsLetterA(word){
+    word = word.toLowerCase()
+    let n = 0;
+
+    for (const letter of word) {
+        if (letter == 'a'){n++;}
+    }
+    return n;
+}
+
+function displayResult(n, word){
+    if (n > 0){
+        console.log(`A letra 'a' aparece ${n} vez(es) na palavra ${word}.`)
+    } else {
+        console.log(`A letra a não aparece na string ${word}.`)
+    }
 }
 
 function __main__(){
     console.log(`Verifique se a letra 'a' existe na string digitada.`)
     console.log(`(O programa executará 10 vezes)`)
     for (i = 10; i > 0; i--){
-        const n = prompt('Digite uma string:');
         
+        const word = prompt('Digite uma string:');
+        numberOfAs = containsLetterA(word)
+        displayResult(numberOfAs, word)
         console.log('')
     }
 }
